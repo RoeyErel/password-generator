3155ef0938685e70b6a0c6928202379287504863
Roey Erel <roee42@gmail.com>
2022-11-06 19:20:59 +0200

    1.2 Build commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,112 @@
+import {useState} from 'react';
+
+
+function generatePassword(length, str) {
+  let resutlt="";
+  for ( var i = 0; i < length; i++ ) {
+    resutlt += str.charAt(Math.floor(Math.random() * (str.length)));
+  }
+  return resutlt
+}
+
 
 function App() {
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz",
+  upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  nums = "0123456789",
+  symbols = '!@#$%^&*()_+',
+  charset = "";
+
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleUpper = event => {
+    if (event.target.checked){
+      charset += upperCase;
+      return charset;
+    }if(!event.target.checked){
+      charset = charset.replace(upperCase, '');
+      return charset;
+    }
+    setIsChecked(current => !current);
+  };
+  const handleLower = event => {
+    if (event.target.checked) {
+      charset += lowerCase;
+      return charset;
+    }if(!event.target.checked){
+      charset = charset.replace(lowerCase, '');
+      return charset;
+    }
+    setIsChecked(current => !current);
+  };
+
+  const handleNumbers = event => {
+    if (event.target.checked) {
+      charset += nums;
+      return charset;
+    }if(!event.target.checked){
+      charset = charset.replace(nums, '');
+      return charset;
+    }
+    setIsChecked(current => !current);
+
+  };
+
+  const handleSymbol = event => {
+    if (event.target.checked) {
+      charset +=symbols;
+      return charset;
+    }if(!event.target.checked){
+      charset = charset.replace(symbols, '');
+      return charset;
+    }
+    setIsChecked(current => !current);
+
+  };
+
+  const handleSubmit = event => {
+    const length = document.getElementById("password").value;
+    let password = generatePassword(length,charset);
+    const post = document.getElementById("result").innerText = password;
+    return password;
+  };
+
   return (
-
-
         <div className="bg-transparent h-full w-full flex justify-center items-center	">
 
           <div className="bg-black bg-opacity-60 w-[80%] h-full flex flex-col items-center">
             <h1 className="text-white text-4xl text-center mt-10">Password Generator</h1>
             <div className="flex justify-start align-center relative text-[18px] py-[12px] px-[10px] h-[50px] w-[70%] bg-black bg-opacity-60 mt-4 mb-[20px] tracking-[1px]">
-              <span className="text-white" id="result">{"pkngwr%@$#ghdsf24532"}</span>
-              <button/> 
+              <span className="text-white" id="result" ></span> 
             </div>
 
             <div className="box-border block w-[70%] text-l text-white">
               
-              <div className="w-full flex justify-between align-center  my-2 mx-0" id="setup">
+              <div className="w-full flex justify-between align-center my-2 mx-0" >
                 <label className="" >Password length</label>
-                <input className="w-[45px] py-[-15px] cursor-pointer" type={"text"}/>
+                <input type="number" id="password" min='4' max='20' className="w-[95px] text-black cursor-pointer"/>
               </div>
 
-              <div className="w-full flex justify-between align-center my-2 mx-0" id="setup">
+              <div className="w-full flex justify-between align-center my-2 mx-0">
                 <label className="">Include uppercase letters</label>
-                <input className="cursor-pointer" type={"checkbox"}/>
+                <input id="uppercase" value={isChecked} onChange={handleUpper} className="cursor-pointer" type="checkbox"/>
               </div>
-              <div  className="w-full flex justify-between align-center my-2 mx-0" id="setup">
+              <div className="w-full flex justify-between align-center my-2 mx-0">
                 <label className="">Include lowercase letters</label>
-                <input className="cursor-pointer" type={"checkbox"}/>
+                <input id="lowercase" value={isChecked} onChange={handleLower} className="cursor-pointer" type="checkbox"/>
               </div>
-              <div className="w-full flex justify-between align-center my-2 mx-0" id="setup">
+              
+              <div className="w-full flex justify-between align-center my-2 mx-0">
                 <label className="">Include numbers</label>
-                <input className="cursor-pointer" type={"checkbox"}/>
+                <input id="numbers" value={isChecked} onChange={handleNumbers} className="cursor-pointer" type="checkbox"/>
               </div>
-              <div  className="w-full flex justify-between align-center my-2 mx-0" id="setup">
+              <div  className="w-full flex justify-between align-center my-2 mx-0">
                 <label className="">Include symbols</label>
-                <input className="cursor-pointer" type={"checkbox"}/>
+                <input id="symbols" value={isChecked} onChange={handleSymbol} className="cursor-pointer" type="checkbox"/>
               </div>
 
               <div className="my-4">
-                <button type="button" className="text-white bg-gradient-to-br from-blue-600 to-green-400 hover:bg-gradient-to-bl  dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                  Submit
-                </button>
+              <input type="submit" value="Submit" onClick={handleSubmit} className="text-white bg-gradient-to-br from-blue-600 to-green-400 hover:bg-gradient-to-bl  dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"/>
               </div>
 
             </div>
